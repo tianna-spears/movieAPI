e7432d99b2af7fff6f57ee2c19d83a2ec7e13cb2
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-11-04 15:11:30 +0000

    final project

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "10df94d0443fdbbb6ce728201f2a8979";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
